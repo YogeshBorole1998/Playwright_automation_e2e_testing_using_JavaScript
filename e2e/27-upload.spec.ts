ca9b209a483bdfc87b8e8a42184d18a55ae1b8cf
Borole <yogesh.borole@capgemini.com>
2024-04-26 23:10:53 +0530

    New Added Code

---
+++ e2e/27-upload.spec.ts
@@ -0,0 +1,9 @@
+import { test, expect } from "@playwright/test";
+
+test.describe("Upload", () => {
+  test("Upload file", async ({ page }) => {
+    await page.goto("https://demoqa.com/upload-download");
+    await page.locator("#uploadFile").setInputFiles(["./download.xlsx"]);
+    await expect(page.locator("#uploadedFilePath")).toBeVisible();
+  });
+});
---
+++ e2e/28-visual.spec.ts
@@ -0,0 +1,11 @@
+import { test, expect } from "@playwright/test";
+
+test.describe("Visual testing", () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto("http://www.saucedemo.com");
+  });
+
+  test("Visual test - login page", async ({ page }) => {
+    await expect(page).toHaveScreenshot({ maxDiffPixels: 100 });
+  });
+});
